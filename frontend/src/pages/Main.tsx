b7cc5c858b30a73da47f9cf4a01be6ff38cfad8f
Shin Young Jei <zerojei0423@naver.com>
2022-10-20 18:03:50 +0900

    사용하지 않는 주석 제거

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -3,23 +3,6 @@ import ApiList from "../componets/main/ApiList";
 import Header from "../componets/main/Header";
 import Start from "../componets/main/Start";
 
-/**
- * Header
- * 메뉴바, 검색, 사용자 정보(프로필 사진)
- * 메뉴바 : 옆으로 슬라이드 효과, [ 내용은 아직 ] + 공통적으로 사용 할 곳
- *         메뉴바를 따로 빼야 할듯
- * 검색 : MainPage한정 ApiList 검색 시 리스트만 제공해줘도 될듯
- * 사용자 정보 : 구글 연동 시 프로필 사진 받아와서 뿌려주기
- *
- * Start
- * 스크롤 화면 꽉 채울 이미지와 구름, 생성하기 버튼
- * 이미지 구름 : 그래픽효과?
- * 생성하기 버튼 : 버튼 클릭 시 API 모달 띄어주기
- *
- * ApiList
- * 현재 활동중인 API 리스트
- */
-
 const Main = () => {
   return (
     <div>
