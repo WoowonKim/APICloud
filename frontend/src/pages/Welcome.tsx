29881181a3162704a673ed17bc3fae7a707921b0
rlamsp234 <rlamsp234@naver.com>
2022-10-21 14:45:15 +0900

    feat: 웰컴 페이지 스크롤시 페이지 전환 적용

--- frontend/src/pages/Welcome.tsx
+++ frontend/src/pages/Welcome.tsx
@@ -1,11 +1,87 @@
-import React from 'react'
-import WelcomeHeader from '../components/NavFooter/WelcomeHeader'
-import './Welcome.scss'
+import React, { useEffect, useRef, useState } from 'react'
+import Dots from '../componets/welcome/Dots';
+import WelcomeHeader from '../componets/welcome/WelcomeHeader'
+import '../componets/welcome/Welcome.scss'
 
 const Welcome = () => {
+  const DIVIDER_HEIGHT = 5;
+  const outerDivRef: any = useRef();
+  const [scrollIndex, setScrollIndex] = useState(1);
+
+  useEffect(() => {
+    const wheelHandler = (e: any) => {
+      e.preventDefault();
+      const {deltaY} = e;
+      const {scrollTop}: any = outerDivRef.current; // 스크롤 위쪽 끝 부분
+      const pageHeight = window.innerHeight; // 화면 세로 길이, 100vh
+
+      if (deltaY > 0) {
+        // 스크롤 내릴 때
+        if (scrollTop >= 0 && scrollTop < pageHeight) {
+          // 현재 1 페이지
+          outerDivRef.current.scrollTo({
+            top: pageHeight + DIVIDER_HEIGHT,
+            left: 0,
+            behavior: "smooth",
+          });
+          setScrollIndex(2);
+        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+          // 현재 2 페이지
+          outerDivRef.current.scrollTo({
+            top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+            left: 0,
+            behavior: "smooth"
+          });
+          setScrollIndex(3);
+        } else {
+          // 현재 3 페이지
+          outerDivRef.current.scrollTo({
+            top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+            left: 0,
+            behavior: "smooth",
+          });
+          setScrollIndex(3);
+        }
+      } else {
+        // 스크롤 올릴 때
+        if (scrollTop >= 0 && scrollTop < pageHeight) {
+          //현재 1페이지
+          outerDivRef.current.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+          });
+          setScrollIndex(1);
+        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+          //현재 2페이지
+          outerDivRef.current.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+          });
+          setScrollIndex(1);
+        } else {
+          // 현재 3페이지
+          outerDivRef.current.scrollTo({
+            top: pageHeight + DIVIDER_HEIGHT,
+            left: 0,
+            behavior: "smooth",
+          });
+          setScrollIndex(2);
+        }
+      }
+    };
+    const outerDivRefCurrent: any = outerDivRef.current;
+    outerDivRefCurrent.addEventListener("wheel", wheelHandler);
+    return () => {
+      outerDivRefCurrent.removeEventListener("wheel", wheelHandler);
+    };
+  }, []);
+
   return (
-    <div>
+    <div ref={outerDivRef} className='outer'>
       <WelcomeHeader />
+      <Dots scrollIndex={scrollIndex} />
       <section className='section1'>
         <h1 className='title'>A&nbsp;&nbsp;P&nbsp;&nbsp;I&nbsp;&nbsp;&nbsp;&nbsp;C&nbsp;&nbsp;l&nbsp;&nbsp;o&nbsp;&nbsp;u&nbsp;&nbsp;d</h1>
         <div className='subTitleWrapper'>
@@ -17,7 +93,7 @@ const Welcome = () => {
           <div className='startButton'>시작하기</div>
         </div>
       </section>
-      <section className='bridge'></section>
+      <div className='divider'></div>
       <section className='section2'>
         <div className='introduction'>
           <div className='title'>
@@ -31,9 +107,18 @@ const Welcome = () => {
           </div>
         </div>
       </section>
-      <section className='bridge'></section>
+      <div className='divider'></div>
       <section className='section3'>
-
+        <div className='introduction'>
+          <div className='title'>
+            <p>밤샘 코딩 하지 마세요!</p>
+            <p>바쁜 개발자들을 위한 API Cloud</p>
+          </div>
+          <div className='subTitle'>
+            <p>동시 편집 기능을 포함한 다양한 서비스가</p>
+            <p>여러분들의 시간을 아껴줍니다.</p>
+          </div>
+        </div>
       </section>
     </div>
   )
