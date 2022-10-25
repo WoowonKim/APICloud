34a7a2323752a51fd41480fe3231125f49493644
eunkyung <bbb381@naver.com>
2022-10-25 13:38:40 +0900

    docs: springboot dockerfile 생성

---
+++ backend/trackingtest/src/main/java/com/web/trackingtest/config/WebClientConfig.java
@@ -0,0 +1,16 @@
+package com.web.trackingtest.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.create("http://localhost:8005/api");
+    }
+}
