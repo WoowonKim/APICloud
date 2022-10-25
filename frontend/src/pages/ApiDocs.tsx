5e76fd302960aa416259075818152ea3ec9eae90
rlamsp234 <rlamsp234@naver.com>
2022-10-25 10:33:56 +0900

    feat: 사이드바 구현

--- frontend/src/pages/ApiDocs.tsx
+++ frontend/src/pages/ApiDocs.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import Sidebar from "../components/ApiDocs/Sidebar";
+import '../components/ApiDocs/ApiDocs.scss'
 
 const ApiDocs = () => {
-  return <div>ApiDocs</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleSide = () => {
+    setIsOpen(true);
+  };
+
+  useEffect(() => {
+    console.log(isOpen)
+  }, [isOpen])
+  
+  return (
+    <div>
+      <div onClick={toggleSide} className="sidebarButton">
+        <FontAwesomeIcon icon={faBars} size="2x" />
+      </div>
+      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+    </div>
+  )
 };
 
 export default ApiDocs;
