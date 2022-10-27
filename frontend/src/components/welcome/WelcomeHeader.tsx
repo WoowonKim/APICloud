88d11f9d6b9998009ecbff56a872c6bdce20ac0a
김우원 <dyffh1031@naver.com>
2022-10-27 17:45:14 +0900

    fix: 웰컴페이지 헤더 버튼 클릭 안되던 버그 수정

--- frontend/src/components/welcome/WelcomeHeader.tsx
+++ frontend/src/components/welcome/WelcomeHeader.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import './WelcomeHeader.scss'
+import React from "react";
+import "./WelcomeHeader.scss";
 
 const WelcomeHeader = () => {
   return (
-    <header className='header'>
-      <div className='container'>
-        <div className='logo'>API Cloud</div>
-        <div className='buttonWrapper'>
-          <div className='signIn'>로그인</div>
-          <div className='signUp'>회원가입</div>
+    <header className="header">
+      <div className="container">
+        <div className="logo">API Cloud</div>
+        <div className="buttonWrapper">
+          <div className="signIn">
+            <a href="/oauth2/authorization/google">구글 로그인</a>
+          </div>
+          <div className="signIn">깃헙 로그인</div>
+          <div className="signUp">회원가입</div>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default WelcomeHeader
+export default WelcomeHeader;
