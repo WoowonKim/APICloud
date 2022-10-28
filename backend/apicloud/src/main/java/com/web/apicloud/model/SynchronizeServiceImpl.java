be8e97bfb58f66a284838ffddde25ada9ea2dc77
eunkyung <bbb381@naver.com>
2022-10-28 17:02:16 +0900

    feat: Method 파싱 로직 구현

--- backend/apicloud/src/main/java/com/web/apicloud/model/SynchronizeServiceImpl.java
+++ backend/apicloud/src/main/java/com/web/apicloud/model/SynchronizeServiceImpl.java
@@ -2,7 +2,9 @@ package com.web.apicloud.model;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -22,7 +24,7 @@ public class SynchronizeServiceImpl implements SynchronizeService {
     public Object getFile(String root, String name) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get("C:/S07P22B309/backend/billow/src/main/java/com/billow/controller/program/ProgramController.java"));
         int i = 0;
-        while(i < lines.size()){
+        while (i < lines.size()) {
             if (KMP(lines.get(i++), REQUEST_MAPPING)) {
                 //TODO : uri 추출해서 저장
                 break;
@@ -30,7 +32,7 @@ public class SynchronizeServiceImpl implements SynchronizeService {
         }
 
         List<String> api = new ArrayList<>();
-        while(i < lines.size()){
+        while (i < lines.size()) {
             if (KMP(lines.get(i), METHOD)) {
                 apiParsing(api);
                 api = new ArrayList<>();
@@ -41,8 +43,22 @@ public class SynchronizeServiceImpl implements SynchronizeService {
     }
 
     private void apiParsing(List<String> api) {
-        System.out.println(api);
-        System.out.println("==============================================");
+        String method = getMethod(api.get(0));
+        if(method == null) return;
+        System.out.println("method" + method.toUpperCase());
+        //public ResponseEntity<Object> selectUser(@RequestHeader("Auth-access") String token) throws IOException {
+    }
+
+    private String getMethod(String str) {
+        int targetIdx1 = str.indexOf("@");
+        int targetIdx2 = str.indexOf(METHOD);
+
+        if (targetIdx1 == -1 || targetIdx2 == -1) return null;
+        String method = str.substring(targetIdx1 + 1, targetIdx2);
+
+        targetIdx1 = str.indexOf("(");
+        targetIdx2 = str.indexOf(")");
+        return method;
     }
 
     static boolean KMP(String parent, String pattern) {
