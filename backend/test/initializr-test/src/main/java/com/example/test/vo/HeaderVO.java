33ffc517e5b48389f49983b1cfefee89b154c601
smj <smjeong53@gmail.com>
2022-10-31 15:59:32 +0900

    add: test 코드에 VO 추가

---
+++ backend/test/initializr-test/src/main/java/com/example/test/vo/HeaderVO.java
@@ -0,0 +1,8 @@
+package com.example.test.vo;
+
+public class HeaderVO {
+
+    private String key;
+
+    private String value;
+}
---
+++ backend/test/initializr-test/src/main/java/com/example/test/vo/ResponseVO.java
@@ -0,0 +1,8 @@
+package com.example.test.vo;
+
+public class ResponseVO {
+
+    private Integer status;
+
+    private PropertyVO responseBody;
+}
