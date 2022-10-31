9376c2157d8a701eca568f48fb78bbe1bfd76d75
smj <smjeong53@gmail.com>
2022-10-31 16:00:47 +0900

    fix: VO data, constructor 관련 annotation 추가

--- backend/apicloud/src/main/java/com/web/apicloud/domain/vo/ServerVO.java
+++ backend/apicloud/src/main/java/com/web/apicloud/domain/vo/ServerVO.java
@@ -1,5 +1,14 @@
 package com.web.apicloud.domain.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ServerVO {
 
     private String version;
