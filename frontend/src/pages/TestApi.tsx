bf7bba3403def1d8c3d15e3fb7ea2e470c8cf639
Shin Young Jei <zerojei0423@naver.com>
2022-11-01 14:19:24 +0900

    refactor: Request Response UI방식이 잘못되어 div 수정

--- frontend/src/pages/TestApi.tsx
+++ frontend/src/pages/TestApi.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "../components/main/Header";
 import ApiAddress from "../components/ApiTest/ApiAddress";
 import ApiBody from "../components/ApiTest/ApiBody";
