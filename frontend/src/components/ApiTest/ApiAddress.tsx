5beabd1b58e1c674f0ebc9e6cc135bbe6f45aa2c
Shin Young Jei <zerojei0423@naver.com>
2022-11-01 16:43:49 +0900

    refactor: 입력창 수정

--- frontend/src/components/ApiTest/ApiAddress.tsx
+++ frontend/src/components/ApiTest/ApiAddress.tsx
@@ -5,7 +5,7 @@ import { RootState } from "../../Store/rootReducer";
 const ApiAddress = () => {
   const isAddress = useSelector((state: RootState) => state.testApi.infomethod);
   return (
-    <div className="apiSide">
+    <div className="apiSideAddress">
       <p className="sideText">사이트 주소 : {isAddress.fixAddress}</p>
       <p className="sideText">공통 URI : {isAddress.commonUri}</p>
     </div>
