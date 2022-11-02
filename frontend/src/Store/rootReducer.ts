90a8588e925b338e45c7189003281de70be54cb4
rlamsp234 <rlamsp234@naver.com>
2022-11-02 15:39:18 +0900

    feat: 메인페이지 DOC 생성 API 연동

--- frontend/src/Store/rootReducer.ts
+++ frontend/src/Store/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import mainApiSlice from "./slice/mainApi";
 import testApiSlice from "./slice/testApi";
 
 const rootReducer = combineReducers({
   testApi: testApiSlice.reducer,
+  mainApi: mainApiSlice.reducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
