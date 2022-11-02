a1698e8d85dbd44f6e57c676a37d6c3b5a9c5bca
smj <smjeong53@gmail.com>
2022-11-02 13:18:57 +0900

    chore: 수정할 JavaSourceCodeWriter 관련 코드 생성

--- backend/apicloud/src/main/java/com/web/apicloud/util/code/ControllerContributor.java
+++ backend/apicloud/src/main/java/com/web/apicloud/util/code/ControllerContributor.java
@@ -4,11 +4,14 @@ import com.web.apicloud.domain.vo.ApiVO;
 import com.web.apicloud.domain.vo.ControllerVO;
 import com.web.apicloud.domain.vo.DocVO;
 import com.web.apicloud.domain.vo.PropertyVO;
+import com.web.apicloud.util.code.java.*;
 import io.spring.initializr.generator.language.Annotation;
 import io.spring.initializr.generator.language.Parameter;
 import io.spring.initializr.generator.language.SourceCodeWriter;
 import io.spring.initializr.generator.language.SourceStructure;
-import io.spring.initializr.generator.language.java.*;
+import io.spring.initializr.generator.language.java.JavaLanguage;
+import io.spring.initializr.generator.language.java.JavaMethodInvocation;
+import io.spring.initializr.generator.language.java.JavaReturnStatement;
 import io.spring.initializr.generator.project.contributor.ProjectContributor;
 import lombok.RequiredArgsConstructor;
 
@@ -25,15 +28,15 @@ import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class ControllerContributor implements ProjectContributor {
-    private final Supplier<JavaSourceCode> sourceFactory;
+    private final Supplier<CustomJavaSourceCode> sourceFactory;
 
-    private final SourceCodeWriter<JavaSourceCode> sourceWriter;
+    private final SourceCodeWriter<CustomJavaSourceCode> sourceWriter;
 
     private final DocVO doc;
 
     @Override
     public void contribute(Path projectRoot) throws IOException {
-        JavaSourceCode sourceCode = this.sourceFactory.get();
+        CustomJavaSourceCode sourceCode = this.sourceFactory.get();
         List<ControllerVO> controllers = doc.getControllers();
         System.out.println(controllers);
         if (controllers != null) {
@@ -43,11 +46,11 @@ public class ControllerContributor implements ProjectContributor {
         this.sourceWriter.writeTo(new SourceStructure(projectRoot.resolve("src/main/"), new JavaLanguage()), sourceCode);
     }
 
-    private Consumer<ControllerVO> controllerConsumer(JavaSourceCode sourceCode) {
+    private Consumer<ControllerVO> controllerConsumer(CustomJavaSourceCode sourceCode) {
         return (controller) -> {
             System.out.println("controllerConsumer() " + controller.getName());
-            JavaCompilationUnit compilationUnit = sourceCode.createCompilationUnit(doc.getServer().getPackageName() + ".controller", controller.getName());
-            JavaTypeDeclaration controllerType = compilationUnit.createTypeDeclaration(controller.getName());
+            CustomJavaCompilationUnit compilationUnit = sourceCode.createCompilationUnit(doc.getServer().getPackageName() + ".controller", controller.getName());
+            CustomJavaTypeDeclaration controllerType = compilationUnit.createTypeDeclaration(controller.getName());
             controllerType.modifiers(Modifier.PUBLIC);
             controllerType.annotate(Annotation.name("org.springframework.web.bind.annotation.RestController"));
             controllerType.annotate(Annotation.name("org.springframework.web.bind.annotation.RequestMapping", builder -> builder.attribute("value", String.class, controller.getCommonUri())));
@@ -58,17 +61,17 @@ public class ControllerContributor implements ProjectContributor {
         };
     }
 
-    private Consumer<ApiVO> apiConsumer(JavaSourceCode sourceCode, JavaTypeDeclaration controllerType) {
+    private Consumer<ApiVO> apiConsumer(CustomJavaSourceCode sourceCode, CustomJavaTypeDeclaration controllerType) {
         return api -> {
             System.out.println("apiConsumer(): " + api.getName());
             addDto(sourceCode, api.getAvailableDTO(), controllerType.getName());
-            JavaMethodDeclaration.Builder builder = JavaMethodDeclaration
+            CustomJavaMethodDeclaration.Builder builder = CustomJavaMethodDeclaration
                     .method(api.getName())
                     .modifiers(Modifier.PUBLIC)
-                    .returning("ResponseEntity<"+ api.getReturning() + ">");
+                    .returning("ResponseEntity<" + api.getReturning() + ">");
             addApiParameters(builder, api);
 
-            JavaMethodDeclaration jmd = builder.body(new JavaReturnStatement(new JavaMethodInvocation("new ResponseEntity<"+ api.getReturning() + ">",
+            CustomJavaMethodDeclaration jmd = builder.body(new JavaReturnStatement(new JavaMethodInvocation("new ResponseEntity<" + api.getReturning() + ">",
                     "", "null")));
 
             String methodMappingName = "org.springframework.web.bind.annotation."
@@ -79,7 +82,7 @@ public class ControllerContributor implements ProjectContributor {
         };
     }
 
-    private void addApiParameters(JavaMethodDeclaration.Builder builder, ApiVO api) {
+    private void addApiParameters(CustomJavaMethodDeclaration.Builder builder, ApiVO api) {
         List<Parameter> parameters = new ArrayList<>();
         // path에서 파라미터 추가
         makeParameters(api.getParameters()).ifPresent(parameters::addAll);
@@ -96,7 +99,7 @@ public class ControllerContributor implements ProjectContributor {
     // TODO: Annotatable한 parameter type 생성하기
     private String makeAnnotation(String s, PropertyVO property) {
         s += "(value = \"" + property.getName() + "\"";
-        if(!property.isRequired()) {
+        if (!property.isRequired()) {
             s += ", required = false";
         }
         s += ")";
@@ -104,7 +107,7 @@ public class ControllerContributor implements ProjectContributor {
     }
 
     private Optional<List<Parameter>> makeParameters(List<PropertyVO> properties) {
-        if(properties == null) {
+        if (properties == null) {
             return Optional.empty();
         }
         return Optional.of(properties.stream()
@@ -115,20 +118,20 @@ public class ControllerContributor implements ProjectContributor {
     }
 
     private Optional<Parameter> makeParameter(PropertyVO property, String annotation) {
-        if(property == null) {
+        if (property == null) {
             return Optional.empty();
         }
         return Optional.of(new Parameter(("".equals(annotation) ? "" : annotation + " ")
                 + property.getTypeForCode(), property.getName()));
     }
 
-    private void addDto(JavaSourceCode sourceCode, List<PropertyVO> dtos, String controllerName) {
+    private void addDto(CustomJavaSourceCode sourceCode, List<PropertyVO> dtos, String controllerName) {
         for (PropertyVO dto : dtos) {
-            JavaTypeDeclaration dtoType = sourceCode.createCompilationUnit(doc.getServer().getPackageName() + ".dto." + controllerName.toLowerCase(), dto.getDtoName()).createTypeDeclaration(dto.getDtoName());
+            CustomJavaTypeDeclaration dtoType = sourceCode.createCompilationUnit(doc.getServer().getPackageName() + ".dto." + controllerName.toLowerCase(), dto.getDtoName()).createTypeDeclaration(dto.getDtoName());
             dtoType.modifiers(Modifier.PUBLIC);
             if (dto.getProperties() != null) {
                 for (PropertyVO property : dto.getProperties()) {
-                    dtoType.addFieldDeclaration(JavaFieldDeclaration.field(property.getName()).modifiers(Modifier.PRIVATE)
+                    dtoType.addFieldDeclaration(CustomJavaFieldDeclaration.field(property.getName()).modifiers(Modifier.PRIVATE)
                             .returning(property.getTypeForCode()));
                 }
             }
---
+++ backend/apicloud/src/main/java/com/web/apicloud/util/code/java/CustomJavaTypeDeclaration.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * source: io.spring.initializr.generator.language.java.CustomJavaTypeDeclaration
+ */
+
+package com.web.apicloud.util.code.java;
+
+import io.spring.initializr.generator.language.TypeDeclaration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomJavaTypeDeclaration extends TypeDeclaration {
+
+	private int modifiers;
+
+	private final List<CustomJavaFieldDeclaration> fieldDeclarations = new ArrayList<>();
+
+	private final List<CustomJavaMethodDeclaration> methodDeclarations = new ArrayList<>();
+
+	CustomJavaTypeDeclaration(String name) {
+		super(name);
+	}
+
+	public void modifiers(int modifiers) {
+		this.modifiers = modifiers;
+	}
+
+	public int getModifiers() {
+		return this.modifiers;
+	}
+
+	public void addFieldDeclaration(CustomJavaFieldDeclaration fieldDeclaration) {
+		this.fieldDeclarations.add(fieldDeclaration);
+	}
+
+	public List<CustomJavaFieldDeclaration> getFieldDeclarations() {
+		return this.fieldDeclarations;
+	}
+
+	public void addMethodDeclaration(CustomJavaMethodDeclaration methodDeclaration) {
+		this.methodDeclarations.add(methodDeclaration);
+	}
+
+	public List<CustomJavaMethodDeclaration> getMethodDeclarations() {
+		return this.methodDeclarations;
+	}
+
+}
