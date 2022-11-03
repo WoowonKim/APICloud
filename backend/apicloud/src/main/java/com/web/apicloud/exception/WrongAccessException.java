70f483c5b73e1fc8d9ad90342d1243d77c9d4e2a
eunkyung <bbb381@naver.com>
2022-11-03 11:36:54 +0900

    feat: Exception 파일 및 ControllerAdvice 파일 생성

---
+++ backend/apicloud/src/main/java/com/web/apicloud/exception/WrongAccessException.java
@@ -0,0 +1,8 @@
+package com.web.apicloud.exception;
+
+public class WrongAccessException extends CommonException {
+
+    public WrongAccessException(String message) {
+        super(message);
+    }
+}
