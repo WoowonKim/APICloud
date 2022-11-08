7013c864457b4b7ba7f12ee5c12d5444d9f4d63a
eunkyung <bbb381@naver.com>
2022-11-08 13:30:45 +0900

    feat: 동기화 Request 요청 생성

--- backend/apicloud/src/main/java/com/web/apicloud/model/SynchronizeService.java
+++ backend/apicloud/src/main/java/com/web/apicloud/model/SynchronizeService.java
@@ -1,10 +1,11 @@
 package com.web.apicloud.model;
 
+import com.web.apicloud.domain.dto.SynchronizeRequest;
 import com.web.apicloud.domain.dto.synchronize.ControllerDTO;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 public interface SynchronizeService {
-    ControllerDTO getFile(Long docId, int controllerId, String name, MultipartFile multipartFile) throws IOException;
+    ControllerDTO getFile(SynchronizeRequest synchronizeRequest, MultipartFile multipartFile) throws IOException;
 }
