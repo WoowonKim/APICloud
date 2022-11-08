292acf6e58d91c767cb2505c5afcb86979d1bb89
Shin Young Jei <zerojei0423@naver.com>
2022-11-08 13:14:26 +0900

    feat: 사용자 API 정보 받아와 헤더 부분 제공 기능 구현

--- frontend/src/components/ApiTest/ApiHeader.tsx
+++ frontend/src/components/ApiTest/ApiHeader.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
+import { RequestTypeInfo } from "../../pages/CreateApi/ApisType";
 import Headerheader from "./Headerheader";
 import HeaderToken from "./HeaderToken";
 
 interface type {
   sideApiList: number;
+  getInfo: RequestTypeInfo | undefined;
 }
 
-const ApiHeader = ({ sideApiList }: type) => {
+const ApiHeader = ({ sideApiList, getInfo }: type) => {
   const [headerTokenFlag, setHeaderTokenFlag] = useState<number | null>(0);
   return (
     <div className="apiHeaderContainer">
@@ -19,15 +21,15 @@ const ApiHeader = ({ sideApiList }: type) => {
       >
         Header
       </span>
-      <span
+      {/* <span
         className={headerTokenFlag === 1 ? "headerClickList" : "headerNoClicklist"}
         onClick={() => {
           setHeaderTokenFlag(1);
         }}
       >
         Token
-      </span>
-      <div className="headerList">{headerTokenFlag === 0 ? <Headerheader sideApiList={sideApiList} /> : <HeaderToken sideApiList={sideApiList} />}</div>
+      </span> */}
+      <div className="headerList">{headerTokenFlag === 0 ? <Headerheader getInfo={getInfo} /> : <HeaderToken sideApiList={sideApiList} />}</div>
     </div>
   );
 };
