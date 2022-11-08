d6fd1ad48d7a1212a117b312458464f101138cd8
smj <smjeong53@gmail.com>
2022-11-08 14:28:50 +0900

    feat: doc 생성, 수정 시 valid한 정보로 서버 정보 수정 후 저장

--- backend/apicloud/src/main/java/com/web/apicloud/domain/dto/CreateDocRequest.java
+++ backend/apicloud/src/main/java/com/web/apicloud/domain/dto/CreateDocRequest.java
@@ -3,6 +3,7 @@ package com.web.apicloud.domain.dto;
 import com.web.apicloud.domain.entity.Docs;
 import com.web.apicloud.domain.entity.Group;
 import com.web.apicloud.domain.vo.UserAuthorityVO;
+import com.web.apicloud.util.TextUtils;
 import lombok.Data;
 
 import java.util.List;
@@ -35,6 +36,25 @@ public class CreateDocRequest {
     private List<UserAuthorityVO> userAuthorityVO;
 
     public Docs toEntity() {
+        return Docs.builder()
+                .docsName(docsName)
+                .serverUrl(serverUrl)
+                .contextUri(contextUri)
+                .javaVersion(javaVersion)
+                .springVersion(springVersion)
+                .buildManagement(buildManagement)
+                .groupPackage(groupPackage)
+                .packageName(packageName)
+                .packaging(packaging)
+                .group(group)
+                .build();
+    }
+
+    public Docs toValidEntity() {
+        String docsName = TextUtils.getValidDocsName(this.docsName);
+        String contextUri = TextUtils.getValidUri(this.contextUri);
+        String groupPackage = TextUtils.getValidGroupPackage(this.groupPackage);
+        String packageName = TextUtils.getValidPackageName(docsName, groupPackage, this.packageName);
         return Docs.builder()
                 .docsName(docsName)
                 .serverUrl(serverUrl)
