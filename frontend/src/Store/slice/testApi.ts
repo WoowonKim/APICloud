58728bb9c56598934b083a8bec84787fb6fb7e37
Shin Young Jei <zerojei0423@naver.com>
2022-11-08 13:11:47 +0900

    feat: Request 서버 정보 받아오기 위한 기능 구현

--- frontend/src/Store/slice/testApi.ts
+++ frontend/src/Store/slice/testApi.ts
@@ -8,23 +8,8 @@ import { RootState } from "../store";
  */
 
 const initialState = {
-  header: {
-    contentType: "application/json",
-    contentLength: "calculated when request is sent",
-    Host: "calculated when request is sent",
-    Accept: "*/*",
-    AcceptEncoding: "gzip, deflate, br",
-    Connection: "keep-alive",
-    Token: "",
-    Cookie: "",
-  },
-  infomethod: {
-    address: "https://localhost:8080",
-    commonUri: "/ApiCloud",
-    userAddress: "https://localhost:8080/ApiCloud/",
-    method: "GET",
-  },
-  body: "",
+  getControllerInfomation: 0,
+  getApisInfomation: 0,
 };
 
 // API 조회 하기.
@@ -41,42 +26,48 @@ const testApiSlice = createSlice({
   name: "testApi",
   initialState,
   reducers: {
-    setUserAddress(state, action) {
-      state.infomethod.userAddress = action.payload.userAddress;
+    addController(state, action) {
+      state.getControllerInfomation = action.payload;
     },
-    setCookie(state, action) {
-      state.header.Cookie = action.payload.cookie;
+    addApis(state, action) {
+      state.getApisInfomation = action.payload;
     },
-    setAddress(state, action) {
-      state.infomethod.address = action.payload.address;
-    },
-    setBody(state, action) {
-      state.body = action.payload.body;
-    },
-    setMethod(state, action) {
-      state.infomethod.method = action.payload.method;
-    },
-    setToken(state, action) {
-      state.header.Token = action.payload.Token;
-    },
-    setContentType(state, action) {
-      state.header.contentType = action.payload.contentType;
-    },
-    setContentLength(state, action) {
-      state.header.contentLength = action.payload.contentLength;
-    },
-    setHost(state, action) {
-      state.header.Host = action.payload.Host;
-    },
-    setAccept(state, action) {
-      state.header.Accept = action.payload.Accept;
-    },
-    setAcceptEncodng(state, action) {
-      state.header.AcceptEncoding = action.payload.AcceptEncoding;
-    },
-    setConnection(state, action) {
-      state.header.Connection = action.payload.Connection;
-    },
+    // setUserAddress(state, action) {
+    //   state.infomethod.userAddress = action.payload.userAddress;
+    // },
+    // setCookie(state, action) {
+    //   state.header.Cookie = action.payload.cookie;
+    // },
+    // setAddress(state, action) {
+    //   state.infomethod.address = action.payload.address;
+    // },
+    // setBody(state, action) {
+    //   state.body = action.payload.body;
+    // },
+    // setMethod(state, action) {
+    //   state.infomethod.method = action.payload.method;
+    // },
+    // setToken(state, action) {
+    //   state.header.Token = action.payload.Token;
+    // },
+    // setContentType(state, action) {
+    //   state.header.contentType = action.payload.contentType;
+    // },
+    // setContentLength(state, action) {
+    //   state.header.contentLength = action.payload.contentLength;
+    // },
+    // setHost(state, action) {
+    //   state.header.Host = action.payload.Host;
+    // },
+    // setAccept(state, action) {
+    //   state.header.Accept = action.payload.Accept;
+    // },
+    // setAcceptEncodng(state, action) {
+    //   state.header.AcceptEncoding = action.payload.AcceptEncoding;
+    // },
+    // setConnection(state, action) {
+    //   state.header.Connection = action.payload.Connection;
+    // },
   },
   extraReducers: {
     [getApiRequestInfo.fulfilled]: (state, action) => {
--- frontend/src/pages/TestApi.tsx
+++ frontend/src/pages/TestApi.tsx
@@ -1,31 +1,45 @@
 import React, { useEffect, useState } from "react";
 import Header from "../components/main/Header";
-import ApiAddress from "../components/ApiTest/ApiAddress";
 import ApiBody from "../components/ApiTest/ApiBody";
 import ApiHeader from "../components/ApiTest/ApiHeader";
 import ApiInputUri from "../components/ApiTest/ApiInputUri";
 import ApiResponse from "../components/ApiTest/ApiResponse";
 import ApiSide from "../components/ApiTest/ApiSide";
 import "../components/ApiTest/ApiTest.scss";
+import { useAppDispatch, useAppSelector } from "../Store/hooks";
+import { mainApi } from "../Store/slice/mainApi";
+import { getApiRequestInfo } from "../Store/slice/testApi";
+import { RequestTypeInfo } from "./CreateApi/ApisType";
 
 const TestApi = () => {
   const [sideApiList, setSidApiList] = useState<number>(0);
+  const [getInfo, setGetInfo] = useState<RequestTypeInfo>();
+  const dispatch = useAppDispatch();
+  const getDocsId = useAppSelector(mainApi);
+
+  // 해당 API정보의 전체를 불러오기.
+  useEffect(() => {
+    dispatch(getApiRequestInfo({ docId: getDocsId.docId })).then((res: any) => {
+      const json = res.payload.detail;
+      const obj = JSON.parse(json);
+      setGetInfo(obj);
+    });
+  }, [getDocsId.docId]);
 
   return (
     <div>
       <Header />
       <div className="testContainer">
         <div className="testSide">
-          <ApiSide setSidApiList={setSidApiList} />
+          <ApiSide getInfo={getInfo} />
         </div>
         <div className="testMain">
           <div className="testInfomation">
-            {/* <ApiAddress /> */}
-            <ApiInputUri sideApiList={sideApiList} />
+            <ApiInputUri getInfo={getInfo} />
           </div>
           <div className="testInfo">
             <div className="testSetting">
-              <ApiHeader sideApiList={sideApiList} />
+              <ApiHeader getInfo={getInfo} sideApiList={sideApiList} />
               <ApiBody sideApiList={sideApiList} />
             </div>
             <ApiResponse />
