b480cdacc8eceeb2d2b1b377535215c748fbca3a
Shin Young Jei <zerojei0423@naver.com>
2022-11-08 16:53:42 +0900

    feat: 사이드바 수정

--- frontend/src/components/main/Header.tsx
+++ frontend/src/components/main/Header.tsx
@@ -5,8 +5,10 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import "./main.scss";
 import { useAppSelector } from "../../Store/hooks";
 import { selectUser } from "../../Store/slice/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
   const [userImg, setUserImg] = useState("");
   const user = useAppSelector(selectUser);
   useEffect(() => {
@@ -14,21 +16,18 @@ const Header = () => {
   }, [user]);
   return (
     <div className="ApiMainHeader">
-      {/* 메뉴바*/}
-      <div className="side">
-        <FontAwesomeIcon className="barsIcon" icon={faBars} />
-      </div>
-
       {/* 로고 */}
-      <img className="logoImg" src={require("../../assets/cloud.png")} />
+      <img
+        className="logoImg"
+        src={require("../../assets/cloud.png")}
+        onClick={() => {
+          navigate("/");
+        }}
+      />
 
       {/* 검색창  */}
       <div className="search">
-        <input
-          className="searchbar"
-          type="text"
-          placeholder="   검색어를 입력하세요"
-        />
+        <input className="searchbar" type="text" placeholder="   검색어를 입력하세요" />
         <button>
           <FontAwesomeIcon icon={faMagnifyingGlass} />
         </button>
@@ -36,7 +35,7 @@ const Header = () => {
 
       {/* 사용자 프로필 */}
       <div className="user">
-        <img className="logoImg" src={userImg} referrerPolicy="no-referrer" />
+        <img className="userImg" src={userImg} referrerPolicy="no-referrer" />
       </div>
     </div>
   );
