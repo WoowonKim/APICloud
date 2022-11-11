f6440c3a27d7ae6c0b37d634521572cd9ddadfab
rlamsp234 <rlamsp234@naver.com>
2022-11-11 16:09:13 +0900

    fix: DOC 서버 정보, Detail 정보 getAPI 수정

--- backend/apicloud/src/main/java/com/web/apicloud/domain/repository/DocsRepository.java
+++ backend/apicloud/src/main/java/com/web/apicloud/domain/repository/DocsRepository.java
@@ -5,7 +5,10 @@ import com.web.apicloud.domain.entity.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DocsRepository extends JpaRepository<Docs, Long> {
     List<Docs> findByGroup(Group group);
+
+    Optional<Docs> findByEncryptedUrl(String encryptedUrl);
 }
