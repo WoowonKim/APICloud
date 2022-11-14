39f84a79c3b16a01d9b786689f9a77d6b85a5e39
김우원 <dyffh1031@naver.com>
2022-11-14 17:04:26 +0900

    feat: 로그아웃 구현 및 비로그인 회원 접근 차단

--- frontend/src/components/welcome/OAuth2RedirectHandler.tsx
+++ frontend/src/components/welcome/OAuth2RedirectHandler.tsx
@@ -2,11 +2,9 @@ import { useEffect } from "react";
 
 export const OAuth2RedirectHandler = () => {
   useEffect(() => {
+    const token = window.location.search.substring(7) as string;
     localStorage.clear();
-    localStorage.setItem(
-      "token",
-      window.location.search.substring(7) as string
-    );
+    localStorage.setItem("token", token);
     window.location.replace("/");
   }, []);
   return <div></div>;
