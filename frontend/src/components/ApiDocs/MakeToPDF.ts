e492cb8c4bd77c7baeec7b299ecc4b32f4908960
rlamsp234 <rlamsp234@naver.com>
2022-11-14 10:00:03 +0900

    feat: API DOC 페이지 문서 PDF 추출

--- frontend/src/components/ApiDocs/MakeToPDF.ts
+++ frontend/src/components/ApiDocs/MakeToPDF.ts
@@ -1,42 +1,31 @@
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 
-const MakeToPDF = () => {
-  const converToImg = async () => {
-    // html to imageFile
-    const paper: any = document.querySelector(".doc");
-    const canvas: any = await html2canvas(paper);
-    const imageFile = canvas.toDataURL("image/png", 6.0);
-    return imageFile
+const MakeToPDF = async () => {
+
+  // html to imageFile
+  const paper: any = document.querySelector(".pdfDocArea");
+  const canvas: any = await html2canvas(paper);
+  const imageFile = canvas.toDataURL("image/png", 6.0);
+  
+  const doc = new jsPDF("p", "mm", "a4")
+  const pageWidth = doc.internal.pageSize.getWidth();
+  const pageHeight = doc.internal.pageSize.getHeight();
+
+  const widthRatio = pageWidth / canvas.width;
+  const customHeight = canvas.height * widthRatio;
+
+  doc.addImage(imageFile, 'png', 0, 0, pageWidth, customHeight);
+  let heightLeft = customHeight;
+  let heightAdd = -pageHeight;
+
+  while (heightLeft >= pageHeight) {
+    doc.addPage()
+    doc.addImage(imageFile, 'png', 0, heightAdd, pageWidth, customHeight);
+    heightLeft -= pageHeight;
+    heightAdd -= pageHeight;
   }
-
-  const converToPdf = (imageFile: any) => {
-    // imageFile to pdf
-
-    const doc = new jsPDF("p", "mm", "a4")
-    const pageWidth = doc.internal.pageSize.getWidth();
-    const pageHeight = doc.internal.pageSize.getHeight();
-
-    doc.addImage(imageFile, "JPEG", 0, 0, pageWidth, pageHeight);
-
-    // doc.save("test.pdf")
-    window.open(doc.output("bloburl"))
-
-    const pdf = new File([doc.output("blob")], "test.pdf", {
-      type: "application/pdf",
-    })
-    return pdf
-  }
-
-  return {
-    viewWithPdf: async () => {
-      // html to imageFile
-      const imageFile = await converToImg()
-
-      // imageFile to Pdf
-      const pdf = converToPdf(imageFile)
-    }
-  }
-};
+  doc.save('filename' + new Date().getTime() + '.pdf');
+}
 
 export default MakeToPDF;
