717878618f458664d8a3d39c65be07f45868072b
ska05142 <ska05142@naver.com>
2022-11-17 16:17:03 +0900

    feat: redux-persist로 user 정보 저장

--- frontend/src/components/main/Header.tsx
+++ frontend/src/components/main/Header.tsx
@@ -4,7 +4,7 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import "./main.scss";
 import { useAppSelector } from "../../Store/hooks";
-import { selectUser } from "../../Store/slice/userSlice";
+import userSlice, { selectUser } from "../../Store/slice/userSlice";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
@@ -55,19 +55,6 @@ const Header = () => {
           navigate("/");
         }}
       />
-
-      {/* 검색창  */}
-      <div className="search">
-        <input
-          className="searchbar"
-          type="text"
-          placeholder="   검색어를 입력하세요"
-        />
-        <button>
-          <FontAwesomeIcon icon={faMagnifyingGlass} />
-        </button>
-      </div>
-
       {/* 사용자 프로필 */}
       <div className="user">
         {user && (
